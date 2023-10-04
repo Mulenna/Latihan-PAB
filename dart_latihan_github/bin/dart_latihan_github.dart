de280483596ab342062494dacc53b561072989de
ichen <nicholascendana@gmail.com>
2023-10-04 12:54:32 +0700

    buat dart

---
+++ dart_latihan_github/bin/dart_latihan_github.dart
@@ -0,0 +1,5 @@
+import 'package:dart_latihan_github/dart_latihan_github.dart' as dart_latihan_github;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_latihan_github.calculate()}!');
+}
---
+++ dart_latihan_github/test/dart_latihan_github_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_latihan_github/dart_latihan_github.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
